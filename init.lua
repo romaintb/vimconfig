c9bb4c033348b5105812b698602fba1fee55796c
Romain Bertrand <romain@doxin.net>
2025-09-05 11:18:16 +0200

    Replace avante with supermaven

--- init.lua
+++ init.lua
@@ -70,15 +70,15 @@ vim.keymap.set('n', '<leader>x', ':qa<cr>')
 
 -- Plugin-specific keybindings (will be set after plugins load)
 vim.keymap.set('n', '<leader>gb', ':Git blame<cr>')
-vim.keymap.set('n', '<leader>ac', '<cmd>AvanteChat<cr>')
-vim.keymap.set('n', '<leader>ae', '<cmd>AvanteEdit<cr>')
-vim.keymap.set('n', '<leader>ax', '<cmd>AvanteExplain<cr>')
-vim.keymap.set('n', '<leader>at', '<cmd>AvanteTest<cr>')
-vim.keymap.set('n', '<leader>as', '<cmd>AvanteStop<cr>')
 vim.keymap.set('n', '<leader>tt', ':Neotree toggle<cr>')
 vim.keymap.set('n', '<leader>tf', ':Neotree reveal<cr>')
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+  vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+  vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+
+  -- Supermaven keymaps
+  vim.keymap.set('n', '<leader>sm', '<cmd>SupermavenToggle<cr>')
+  vim.keymap.set('n', '<leader>sr', '<cmd>SupermavenRestart<cr>')
+  vim.keymap.set('n', '<leader>ss', '<cmd>SupermavenStatus<cr>')
 
 -- Autocmds
 vim.api.nvim_create_autocmd("FileType", {
@@ -238,132 +238,26 @@ require("lazy").setup({
     end,
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- API configuration
-      provider = "claude", -- Updated to use "claude" instead of "anthropic"
-      auto_suggestions = true,
-      providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-3-5-sonnet-20241022",
-          extra_request_body = {
-            temperature = 0,
-            max_tokens = 4096,
-          },
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<Tab>",
+          clear_suggestion = "<C-]>",
+          accept_word = "<C-j>",
         },
-      },
-      behaviour = {
-        auto_suggestions = false,
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
-      },
-      mappings = {
-        --- @class AvanteConflictMappings
-        diff = {
-          ours = "co",
-          theirs = "ct",
-          all_theirs = "ca",
-          both = "cb",
-          cursor = "cc",
-          next = "]x",
-          prev = "[x",
+        ignore_filetypes = {},
+        color = {
+          suggestion_color = "#ffffff",
+          cterm = 244,
         },
-        suggestion = {
-          accept = "<M-l>",
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-        jump = {
-          next = "]]",
-          prev = "[[",
-        },
-        submit = {
-          normal = "<CR>",
-          insert = "<C-s>",
-        },
-        sidebar = {
-          apply_all = "A",
-          apply_cursor = "a",
-          switch_windows = "<Tab>",
-          reverse_switch_windows = "<S-Tab>",
-        },
-      },
-      hints = { enabled = true },
-      windows = {
-        position = "right", -- the position of the sidebar
-        wrap = true, -- similar to vim.o.wrap
-        width = 30, -- default % based on available width
-        sidebar_header = {
-          align = "center", -- left, center, right for title
-          rounded = true,
-        },
-      },
-      highlights = {
-        ---@type AvanteConflictHighlights
-        diff = {
-          current = "DiffText",
-          incoming = "DiffAdd",
-        },
-      },
-      --- @class AvanteConflictUserConfig
-      diff = {
-        autojump = true,
-        ---@type string | fun(): string
-        list_opener = "copen",
-      },
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
+        log_level = "info",
+        disable_inline_completion = false,
+        disable_keymaps = false,
+      })
+    end,
   },
-  -- {
-  --   "github/copilot.vim",
-  --   lazy = true,
-  -- },
-  -- {
-  --   "Exafunction/codeium.vim",
-  --   lazy = true,
-  -- },
+
   -- {
   --   "leafgarland/typescript-vim",
   --   lazy = true,
