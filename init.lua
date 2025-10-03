75f4984c0c20ac1cc9fa8130a63c21a5d15d1bc7
Romain Bertrand <romain@doxin.net>
2025-10-03 12:13:25 +0200

    Use snacks picker

--- init.lua
+++ init.lua
@@ -72,10 +72,7 @@ vim.keymap.set('n', '<leader>w', ':w<cr>')
 vim.keymap.set('n', '<leader>q', ':q<cr>')
 vim.keymap.set('n', '<leader>x', ':qa<cr>')
 
--- Plugin-specific keybindings (will be set after plugins load)
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
--- Snacks keybindings are defined in the plugin config
+-- Plugin-specific keybindings are defined in the plugin configs
 
 -- LSP keybindings (will be available when LSP attaches)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
@@ -123,15 +120,31 @@ require("lazy").setup({
       git = { enabled = true },
       gitbrowse = { enabled = true },
       rename = { enabled = true },
-      dashboard = { enabled = false }, -- Disable if you don't want a dashboard
+      dashboard = { enabled = true },
+      picker = { enabled = true }
     },
     keys = {
+      -- File pickers
+      { "<leader>ff", function() Snacks.picker.files() end,                             desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.grep() end,                              desc = "Grep" },
+      { "<leader>fb", function() Snacks.picker.buffers() end,                           desc = "Buffers" },
+      { "<leader>fr", function() Snacks.picker.recent() end,                            desc = "Recent Files" },
+      { "<leader>:",  function() Snacks.picker.command_history() end,                   desc = "Command History" },
+      { "<leader>/",  function() Snacks.picker.grep_buffers() end,                      desc = "Grep Buffers" },
+
+      -- Git
       { "<leader>lg", function() Snacks.lazygit() end,                                  desc = "LazyGit" },
-      { "<leader>tt", function() Snacks.explorer() end,                                 desc = "Toggle Explorer" },
-      { "<leader>tf", function() Snacks.explorer({ cwd = vim.fn.expand("%:p:h") }) end, desc = "Explorer (current file)" },
+      { "<leader>gg", function() Snacks.lazygit() end,                                  desc = "LazyGit" },
       { "<leader>gb", function() Snacks.git.blame_line() end,                           desc = "Git Blame Line" },
       { "<leader>gB", function() Snacks.gitbrowse() end,                                desc = "Git Browse" },
-      { "<leader>gg", function() Snacks.lazygit() end,                                  desc = "LazyGit" },
+      { "<leader>gc", function() Snacks.picker.git_log() end,                           desc = "Git Log" },
+      { "<leader>gs", function() Snacks.picker.git_status() end,                        desc = "Git Status" },
+
+      -- Explorer
+      { "<leader>tt", function() Snacks.explorer() end,                                 desc = "Toggle Explorer" },
+      { "<leader>tf", function() Snacks.explorer({ cwd = vim.fn.expand("%:p:h") }) end, desc = "Explorer (current file)" },
+
+      -- Utilities
       { "<leader>.",  function() Snacks.scratch() end,                                  desc = "Toggle Scratch Buffer" },
       { "<leader>S",  function() Snacks.scratch.select() end,                           desc = "Select Scratch Buffer" },
       { "<c-/>",      function() Snacks.terminal() end,                                 desc = "Toggle Terminal" },
@@ -143,17 +156,6 @@ require("lazy").setup({
   {
     "tpope/vim-fugitive",
     cmd = { "Git", "Gstatus", "Gblame", "Gpush", "Gpull" },
-  },
-
-  -- File navigation
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "Telescope",
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live grep" },
-    },
   },
 
   -- Text editing enhancements
